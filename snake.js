18030cfb5e3365769468b28da8f50d279b3d9b84
lancaster2953 <80279524+lancaster2953@users.noreply.github.com>
2025-11-28 23:00:39 -0700

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,5 +1,5 @@
 const Snake = {
-  position: {x: 0, y: 0},
+  position: {x: 5, y: 0},
   velocity: {x: 1, y: 0},
   draw(context){
     context.fillRect(this.position.x * CELL_SIZE, this.position.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
