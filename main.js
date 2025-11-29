a5b21aa1850bb408aae6da7ef6ff52064b278232
lancaster2953 <80279524+lancaster2953@users.noreply.github.com>
2025-11-28 23:02:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function(){
   const canvas = document.getElementById('canvas1');
-  const cts = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d');
   canvas.width = GAME.width;
   canvas.height = GAME.height;
 
