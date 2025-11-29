a0b335aa8b02b9ec53b2673ee65b208699f77baa
lancaster2953 <80279524+lancaster2953@users.noreply.github.com>
2025-11-28 23:34:45 -0700

    Update snake.js

--- snake.js
+++ snake.js
@@ -4,7 +4,7 @@ const Snake = {
   image: document.getElementById('zhu_di'),
   draw(context){
     context.fillRect(this.position.x * CELL_SIZE, this.position.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-    context.drawImage(this.image.x * CELL_SIZE, this.image.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    context.drawImage(this.image, CELL_SIZE, CELL_SIZE);
   },
   update(){
     this.position.x += this.velocity.x;
